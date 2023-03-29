ee3aaa3ebf0fc2f527b2937a2017965654b86ba5
TonyKaku <Tonykaku@yeah.yeah>
2023-03-29 22:03:06 +0200

    upd

--- apps/locale/locales.js
+++ apps/locale/locales.js
@@ -185,8 +185,8 @@ var locales = {
     datePattern: { 0: "%Y/%m/%d", 1: "%y/%m/%d" },
     abmonth: "Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec",
     month: "January,February,March,April,May,June,July,August,September,October,November,December",
-    abday: "SunµMonµTueµWedµThuµFriµSat",
-    day: "SundayµMondayµTuesdayµWednesdayµThursdayµFridayµSaturday",
+    abday: "SunµMonµTueµ火曜µThuµFriµSat",
+    day: "SundayµMondayµTuesdayµ火曜日µThursdayµFridayµSaturday",
     // No translation for english...
   },
   "nl_NL": {
